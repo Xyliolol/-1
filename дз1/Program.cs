5f509e8ff5383b70bf967e4f0be05979de9c83f1
Deo <jeeytis@yandex.ru>
2021-10-28 15:10:15 +0300

    Добавьте файлы проекта.

---
+++ дз1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace дз1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name} сегодня [{DateTime.Now}] !");
+        }
+    }
+}
