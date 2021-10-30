b589e0989e856f0b2e4d484188428aa9946511a8
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2021-10-30 16:43:51 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,69 +1,22 @@
 ﻿using System;
 
-namespace дз2._2
+namespace дз2._3
 {
     class Program
     {
-        
-
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите текущий месяц");
-            int i = Convert.ToInt32(Console.ReadLine());
-            int Month = Convert.ToInt32(DateTime.Now.ToString("MM"));
-            string MonthTitle = "";
-            switch (Month)
+            int Enter = Convert.ToInt32(Console.ReadLine());
+            if (Enter % 2 == 0)
             {
-              
-                case 1:
-                    MonthTitle = "Январь";
-                    break;
-                case 2:
-                    MonthTitle = "Февраль";
-                    break;
-                case 3:
-                    MonthTitle = "Март";
-                    break;
-                case 4:
-                    MonthTitle = "Апрель";
-                    break;
-                case 5:
-                    MonthTitle = "Май";
-                    break;
-                case 6:
-                    MonthTitle = "Июнь";
-                    break;
-                case 7:
-                    MonthTitle = "Июль";
-                    break;
-                case 8:
-                    MonthTitle = "Август";
-                    break;
-                case 9:
-                    MonthTitle = "Сентябрь";
-                    break;
-                case 10:
-                    MonthTitle = "Октябрь";
-                    break;
-                case 11:
-                    MonthTitle = "Ноябрь";
-                    break;
-                case 12:
-                    MonthTitle = "Декабрь";
-                    break;
+                Console.WriteLine("Ваше число делится на 2 без остатка");
+            }
+            else
+            {
+                Console.WriteLine("Ваше число НЕ делится на 2 без остатка");
             }
 
-            Console.WriteLine(MonthTitle);
 
-            
-
-            
         }
     }
 }
-
-
-    
-
-
