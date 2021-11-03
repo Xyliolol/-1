8c705ca5ef2635fa2086bdd215f9bcd830962c8b
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2021-11-03 15:45:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace дз3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "Hello";
+            var array = str.ToCharArray();
+
+
+            array[4] = 'H';
+            array[3] = 'e';
+            array[2] = 'l';
+            array[1] = 'l';
+            array[0] = 'o';
+
+
+            str = new string(array);
+            Console.WriteLine(str);
+
+        }
+    }
+}
