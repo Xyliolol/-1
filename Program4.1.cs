845d10aa25d4a40e2d4e0fe1eb106ce0be587d8f
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2021-11-08 12:57:51 +0300

    Add files via upload

---
+++ Program4.1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace дз4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFullName());
+            Console.WriteLine(GetFullName());
+            Console.WriteLine(GetFullName());
+
+        }
+        static (string firstName, string lastName, string patronymic) GetFullName()
+        {
+            Console.WriteLine("Ведите имя");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Введите отчество");
+            string patronymic = Console.ReadLine();
+            return (firstName,lastName,patronymic);
+        }
+        
+
+
+    }
+    
+}
+
