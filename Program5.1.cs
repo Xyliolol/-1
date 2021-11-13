ba2342aed060e7677183927ab8fa59f0018203bf
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2021-11-13 22:42:28 +0300

    Add files via upload

---
+++ Program5.1.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System;
+
+namespace дз_5._1
+{
+
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string filename = "text.txt";
+            File.WriteAllText(filename,$"{text}");
+        }
+    }
+}
+
+
+
+
+
+
+    
---
+++ Program5.3.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+
+namespace дз5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива:\t");
+            byte elements = byte.Parse(Console.ReadLine());
+            byte[] array = new byte[elements];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}:\t");
+                array[i] = byte.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Вывод массива");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            File.WriteAllBytes("bytes.bin", array);
+            byte[] fromFile = File.ReadAllBytes("bytes.bin");
+
+        }
+
+
+    }
+}
